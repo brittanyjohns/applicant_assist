343f4e5dd95cce4ac58014c9253375ca0b01e771
Brittany Johns <britt@Brittanys-MacBook-Air.local>
2023-09-16 12:31:07 -0400

    fix js imports - i hope

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "trix";
-import "@rails/actiontext";
-import "@hotwired/turbo-rails";
-import "controllers";
-import * as bootstrap from "bootstrap";
