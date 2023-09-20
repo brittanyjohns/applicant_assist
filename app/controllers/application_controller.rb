ad9541722598609907cc88510a3a0f6d568780ea
Brittany Johns <britt@Brittanys-MacBook-Air.local>
2023-09-19 22:08:29 -0400

    protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
-  # protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
   helper_method :current_order
   before_action :set_categories
