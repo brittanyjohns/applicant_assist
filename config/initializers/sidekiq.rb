415f9dac8782dfa8b1106082a02f29a70b3df8d9
Brittany Johns <britt@Brittanys-MacBook-Air.local>
2023-10-03 11:24:11 -0400

    add redis connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,11 @@
 Sidekiq.configure_server do |config|
-  #   config.logger = Sidekiq::Logger.new($stdout)
-  config.logger = Rails.logger
+  logger = ActiveSupport::Logger.new("log/sidekiq_#{Rails.env}.log")
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+  config.log_level = :debug
+  config.redis = { url: ENV['REDIS_URL'] }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV['REDIS_URL'] }
 end
