2be1041e7a85b29938c7d3bcec9ad7d86e59f741
Brittany Johns <britt@Brittanys-MacBook-Air.local>
2023-10-03 11:44:30 -0400

    rm log formatter

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,10 @@
 Sidekiq.configure_server do |config|
   logger = ActiveSupport::Logger.new("log/sidekiq_#{Rails.env}.log")
-  logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
   config.log_level = :debug
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = { url: ENV["REDIS_URL"] }
 end
